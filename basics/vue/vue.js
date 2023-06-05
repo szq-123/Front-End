e8275b20067d8ab201b146ac179f4d367e6c4eee
szq-123 <2863301875@qq.com>
2023-06-05 21:55:10 +0800

    start with vue

--- basics/vue/vue.js
+++ basics/vue/vue.js
@@ -1,2 +1,5 @@
 // JS framework. simplify DOM operations.
 // see more at https://vuejs.org/
+
+// vite: a build tool.
+// compared to `webpack`, the build to when using `vue create demo` like in vue2.
